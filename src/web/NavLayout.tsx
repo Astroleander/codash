a4ab20f9507533853f6b4b3c12b965b0e1eafa15
Astroleander <astroleander@gmail.com>
2020-11-07 16:03:16 +0800

    [ui] replace with real item

--- src/web/NavLayout.tsx
+++ src/web/NavLayout.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { generateDisplayName } from '../utils/generators';
+import ListItem from './components/ListItem';
 import { routeMeta } from './router/RouteMeta';
 import { useHashRouter } from '../utils/useRouter';
 
 const RouteList = () => {
   const { routes, go } = useHashRouter(routeMeta);
-  return (<>{routes.map(meta =>
-    <li onClick={() => go(meta.path)} key={meta.path}>{ generateDisplayName(meta) }</li>
-  )}</>);
+  return (<ul>{routes.map(meta =>
+    <li onClick={() => go(meta.path)} key={meta.path}><ListItem meta={meta}/></li>
+  )}</ul>);
 }
 
 export default function () {
   return (<>
     <RouteList />
-  </>)
+  </>);
 };
