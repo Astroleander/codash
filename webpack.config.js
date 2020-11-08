4c5cd0b1f37304596e556a222a4cfed5d9219228
Astroleander <astroleander@gmail.com>
2020-11-08 12:49:58 +0800

    [feat] support less

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const StandardCssLoader = [
+  { loader: MiniCssExtractPlugin.loader },
+  {
+    loader: 'css-loader',
+    options: {
+      modules: { auto: true, localIdentName: '[local]_MODULE_[contenthash:6]' }
+    }
+  }
+];
 
 module.exports = {
   mode: 'development',
   entry: './src/index.tsx',
   target: 'web',
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].bundle.[contenthash:6].js',
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
@@ -18,13 +29,27 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: ['/node-modules/']
+      },
+      {
+        test: /\.css$/,
+        use: StandardCssLoader,
+      },
+      {
+        test: /\.less$/,
+        use: StandardCssLoader.concat(
+          { loader: 'less-loader' }
+        )
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html')
-    })
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash:6].css",
+      chunkFilename: "chunk.[id].[contenthash:6].css",
+    }),
   ],
   devtool: 'inline-source-map',
   devServer: {
