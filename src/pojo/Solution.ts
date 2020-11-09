d0e9bbc17715c1c9fed54bf823637bd5d078b4d2
Astroleander <astroleander@gmail.com>
2020-11-09 16:09:20 +0800

    [feat] add type inference for configs

--- src/pojo/Solution.ts
+++ src/pojo/Solution.ts
@@ -1,7 +1,12 @@
 const SolutionWrapper = {
-  create: (func: Function, params: any[], content = null) => {
+  create: (func: Function, params: any[], content:Configs|null = null) => {
     return new Solution(func, params, content);
   }
+}
+
+interface Configs {
+  outputType?: RESULT_TYPE;
+  inputType?: INPUT_TYPE;
 }
 
 class Solution {
@@ -9,7 +14,7 @@ class Solution {
   params: any[];
   configs: object;
 
-  constructor(func: Function, params: any[], content: {}|null) {
+  constructor(func: Function, params: any[], content: Configs|null) {
     this.solution = func;
     this.params = params;
     this.configs = content;
@@ -25,7 +30,12 @@ enum RESULT_TYPE {
   TREE,
 }
 
+enum INPUT_TYPE {
+  MUTABLE,
+}
+
 export {
   SolutionWrapper as Solution,
   RESULT_TYPE,
+  INPUT_TYPE,
 };
