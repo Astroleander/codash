21054573dd7e5e60958bf7fd756469fbb0580d5a
Astroleander <astroleander@gmail.com>
2020-11-11 15:06:14 +0800

    [feat] add tags

--- src/web/components/ListItem/Tag.tsx
+++ src/web/components/ListItem/Tag.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from './tag.module.less';
+import cls from 'classnames';
+
+console.log(styles)
 
 export default function Tag({ text } : { text: string }) {
-  return (<span className={styles.tag}>
-    {text}
+  
+  return (<span className={cls(styles.tag, styles['tag-' + text])}>
+    {text.replace(/_/g, ' ')}
   </span>);
 }
